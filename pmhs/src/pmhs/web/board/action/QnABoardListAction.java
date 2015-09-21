6bf6dde9d539b31a3fbdabbb9153bf9adb1e91d4
baejaeeon <baejaeeon2@naver.com>
2015-09-21 14:39:42 +0900

    package commit

--- pmhs/src/pmhs/web/board/action/QnABoardListAction.java
+++ pmhs/src/pmhs/web/board/action/QnABoardListAction.java
@@ -1,5 +1,16 @@
 package pmhs.web.board.action;
 
-public class QnABoardListAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import pmhs.action.Action;
+import pmhs.vo.ActionForward;
+
+public class QnABoardListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- pmhs/src/pmhs/web/boardAdmin/action/NoticeBoardListAction.java
+++ pmhs/src/pmhs/web/boardAdmin/action/NoticeBoardListAction.java
@@ -1,5 +1,16 @@
 package pmhs.web.boardAdmin.action;
 
-public class NoticeBoardListAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import pmhs.action.Action;
+import pmhs.vo.ActionForward;
+
+public class NoticeBoardListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

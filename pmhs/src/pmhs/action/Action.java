228ac9324a44f5f03fc03fa4e6b1fadf3a8889c2
baejaeeon <baejaeeon2@naver.com>
2015-09-21 12:05:33 +0900

    PC Manage Helper System

---
+++ pmhs/src/pmhs/action/Action.java
@@ -0,0 +1,13 @@
+package pmhs.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pmhs.vo.ActionForward;
+
+//각각의 요청을 처리하는 Action클래스 객체들의 규격을 정의한 인터페이스
+public interface Action {
+	
+	public ActionForward execute(HttpServletRequest request,
+			HttpServletResponse response) throws Exception;
+}

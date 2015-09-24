9105d015cc6558f8a45f503878a7615f7c2e32b4
baejaeeon <baejaeeon2@naver.com>
2015-09-24 18:45:30 +0900

    index commit

--- pmhs/src/pmhs/web/member/action/loginProAction.java
+++ pmhs/src/pmhs/web/member/action/loginProAction.java
@@ -18,7 +18,6 @@ public class loginProAction implements Action {
 		// TODO Auto-generated method stub
 		String id = request.getParameter("id");
 		String passwd = request.getParameter("passwd");
-
 		LoginService loginService = new LoginService();
 		Member loginUser = loginService.getLoginUser(id, passwd);
 
